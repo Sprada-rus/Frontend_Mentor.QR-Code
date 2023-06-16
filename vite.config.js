6c847782c3df30642de563966c91b7e522592b88
Sprada <sappronov@gamil.com>
2023-06-16 18:06:43 +0300

    add config vite and upd style

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
 import { defineConfig } from "vite";
 
 export default defineConfig({
-    server: {
-        open: '/src/index.html'
+    root: 'src',
+    build: {
+        outDir: '../docs',
     }
 })
